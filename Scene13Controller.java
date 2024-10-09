ca6a10c3ec5ccea51d24d62dc46ad12c592f03a1
Yu-Okamura <147110529+Yu-Okamura@users.noreply.github.com>
2024-10-09 16:08:13 -0700

    temp scene 13 (admin post login)

---
+++ Scene13Controller.java
@@ -0,0 +1,37 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class Scene13Controller {
+
+    @FXML
+    private Hyperlink signOutLink;
+
+    @FXML
+    private void handleSignOut(ActionEvent event) {
+        switchScene(event, "scene1.fxml");
+    }
+
+    private void switchScene(ActionEvent event, String fxmlFile) {
+        try {
+            // Load the FXML for the new scene
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
+            Parent root = loader.load();
+
+            // Get the current stage (window) and set the new scene
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root, 600, 400));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
